861c7e2256a3f046bde5611af8aade3223209d6e
Stanley Ugwu <stanleyugwu2018@gmail.com>
2021-07-20 10:47:53 -0700

    Header and Navigation Done

--- app/client/src/components/Header.jsx
+++ app/client/src/components/Header.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navigation from './Navigation';
 import s from '../styles/Header.module.css';
+import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { faBars, faSun, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Header = (props) => {
+
+    const [menuVisible, setMenuVisible] = useState(false);
+
     return (
-        <header className={s.header}>
-            HEADER
+        <header className={'pt-2 mb-5 border-none border-bottom px-lg-5 pb-3'}>
+            <div className="row">
+                <div className="col-2 d-flex align-items-center">
+                    <h3>Shortnr</h3>
+                </div>
+
+                <div className="col-10 d-flex align-items-center justify-content-end">
+                    <Icon icon={faSun} mask={['far', 'circle']} className="fs-3 d-inline me-3"/>
+                    <div className="d-inline d-sm-none p-2 border pb-1">
+                        <Icon icon={menuVisible ? faTimes : faBars} mask={['far', 'circle']} className="text-primary fs-2" onClick={(evt) => {setMenuVisible(!menuVisible)}}/>
+                    </div>
+                    <div className="text-center d-none d-sm-inline">
+                        <Navigation mobileView={false}/>
+                    </div>
+                    
+                </div>
+            </div>
+            {
+                menuVisible &&
+                <div className="pt-2 menu-wrapper d-block d-sm-none">
+                    <Navigation mobileView={true}/>
+                </div>
+            }
+            
         </header>
     )
 }
---
+++ app/client/src/components/Navigation.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const NAV_ITEMS = [
+    {text:'About Us',url:'/about.html'},
+    {text:'Login',url:'/login'}, 
+    {text:'Sign Up',url:'/signup'}
+]
+
+const Navigation = (props) => {
+    let {mobileView} = props;
+
+    return (
+        <nav className={mobileView ? 'd-block w-100 bg-primary' : 'd-inline'}>
+            <ul className="pb-0 list-inline d-inline">
+                {
+                    NAV_ITEMS.map(item => {
+                        let {text, url} = item;
+                        return (
+                            <li className={mobileView ? 'd-block pb-1 text-center' : 'd-inline pe-2'}>
+                                {
+                                    mobileView ? (
+                                        <Button href={url} color="white">{text}</Button>
+                                    ) : (
+                                        <Button
+                                            href={text == 'Login' || text == 'Sign Up' ? false : url}
+                                            variant={text == 'Login' ? 'outline-primary' : text == 'Sign Up' ? 'primary' : false}
+                                            className={text == 'Sign Up' ? 'text-white' : 'text-primary'}
+                                        >
+                                            {text}
+                                        </Button>
+                                    )
+                                }
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
